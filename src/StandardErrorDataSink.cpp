2f8d2f21559b7250a8c0664813c26b810e18b804
Charitha Dendhi <cdendhi@ucdavis.edu>
2026-08-22 17:51:11 -0700

    Remove StandardErrorDataSink implementation
    Removed implementation of CStandardErrorDataSink methods.

--- src/StandardErrorDataSink.cpp
+++ src/StandardErrorDataSink.cpp
@@ -1,12 +1,1 @@
-#include "StandardErrorDataSink.h"
-#include <iostream>
-
-bool CStandardErrorDataSink::Put(const char &ch) noexcept{
-    std::cerr.put(ch);
-    return std::cerr.good();
-}
-
-bool CStandardErrorDataSink::Write(const std::vector<char> &buf) noexcept{
-    std::cerr.write(buf.data(),buf.size());
-    return std::cerr.good();
-}
+// Source code is unavailable due to academic integrity purposes. Available upon request. 
