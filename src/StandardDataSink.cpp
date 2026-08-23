5423730f6d1bd26894f064e8f268da1e6fddec7e
Charitha Dendhi <cdendhi@ucdavis.edu>
2026-08-22 17:50:53 -0700

    Update StandardDataSink.cpp

--- src/StandardDataSink.cpp
+++ src/StandardDataSink.cpp
@@ -1,12 +1,1 @@
-#include "StandardDataSink.h"
-#include <iostream>
-
-bool CStandardDataSink::Put(const char &ch) noexcept{
-    std::cout.put(ch);
-    return std::cout.good();
-}
-
-bool CStandardDataSink::Write(const std::vector<char> &buf) noexcept{
-    std::cout.write(buf.data(),buf.size());
-    return std::cout.good();
-}
+// Source code is unavailable due to academic integrity purposes. Available upon request. 
