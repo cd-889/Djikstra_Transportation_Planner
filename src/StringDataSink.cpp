e6ee429fa6fe47d393915c4c37bf9d6414615b1b
Charitha Dendhi <cdendhi@ucdavis.edu>
2026-08-22 17:51:21 -0700

    Remove CStringDataSink method implementations
    Removed implementation of CStringDataSink methods.

--- src/StringDataSink.cpp
+++ src/StringDataSink.cpp
@@ -1,15 +1,1 @@
-#include "StringDataSink.h"
-
-const std::string &CStringDataSink::String() const{
-    return DString;
-}
-
-bool CStringDataSink::Put(const char &ch) noexcept{
-    DString += std::string(1,ch);
-    return true;
-}
-
-bool CStringDataSink::Write(const std::vector<char> &buf) noexcept{
-    DString += std::string(buf.data(),buf.size());
-    return true;
-}
+// Source code is unavailable due to academic integrity purposes. Available upon request. 
