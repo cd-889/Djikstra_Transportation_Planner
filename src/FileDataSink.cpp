cbe742a5e69be082ade33171d7dd13b8e340f6db
Charitha Dendhi <cdendhi@ucdavis.edu>
2026-08-22 17:49:59 -0700

    Update FileDataSink.cpp

--- src/FileDataSink.cpp
+++ src/FileDataSink.cpp
@@ -1,16 +1,1 @@
-#include "FileDataSink.h"
-
-
-CFileDataSink::CFileDataSink(const std::string &filename){
-    DFile.open(filename);
-}
-
-bool CFileDataSink::Put(const char &ch) noexcept{
-    DFile.put(ch);
-    return DFile.good();
-}
-
-bool CFileDataSink::Write(const std::vector<char> &buf) noexcept{
-    DFile.write(buf.data(),buf.size());
-    return DFile.good();
-}
+// Source code is unavailable due to academic integrity purposes. Available upon request. 
